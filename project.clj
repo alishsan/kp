473f2951c3a5d0acdbb7abc0ceb2884aec2ada78
Alisher Sanetullaev <alishsan@gmail.com>
2025-08-23 13:33:30 +0500

    MIT license

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject kp "0.1.0-SNAPSHOT"
   :description "Kronig–Penney model CLI in Clojure"
-  :url "https://example.com/kp"
-  :license {:name "MIT"}
+  :url "https://github.com/YOUR_USERNAME/kp"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.cli "1.1.230"]
                  [org.clojure/data.csv "1.0.1"]]
