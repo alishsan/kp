e0a70b41f769d97568628b6af72b7ab86b29c9c9
Alisher Sanetullaev <alishsan@gmail.com>
2025-08-23 13:35:48 +0500

    Update GitHub URL with actual username

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kp "0.1.0-SNAPSHOT"
   :description "Kronig–Penney model CLI in Clojure"
-  :url "https://github.com/YOUR_USERNAME/kp"
+  :url "https://github.com/alishsan/kp"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
